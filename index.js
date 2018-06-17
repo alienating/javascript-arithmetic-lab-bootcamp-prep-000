a6f01cde0dad441fc5c240faf7a515ae84cc3bc0
Axel Jenson <hello@designbyeden.com>
2018-06-17 15:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,22 +22,3 @@ function dec(n) {
   return n -= 1
 }
 
-function makeInt(a) {
-  return parseInt(n)
-}
-
-function makeInt(b) {
-  return parseInt(n, 10)
-}
-
-function makeInt(c) {
-  return parseInt('nonsense!', 10)
-}
-
-function preserveDecimal(a) {
-  return parseFloat(2.222)
-}
-
-function preserveDecimal(b) {
-  return parseFloat('nonsense!')
-}
